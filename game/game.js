4ace04fe240671a951895331deb1a52faa3593e8
omthegg <iliya.pashapour@gmail.com>
2022-01-09 15:59:35 +0330

    Update

--- game/game.js
+++ game/game.js
@@ -1,1 +1,22 @@
-const stage = acgraph.create('stage-container');
+var playerX = 185
+var playerY = 185
+var playerSpeed = 100
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(48)
+}
+
+function draw() {
+  background(0, 0, 0)
+
+  fill(255, 255, 255)
+  rect(playerX, playerY, 30, 30)
+}
+
+function keyPressed() {
+  if (keyCode == W) {
+    playerY -= playerSpeed * deltaTime;
+    alert("Key w pressed")
+  }
+}

e1a00bd0a99911337b7feab34b7047733ac45091
omthegg <iliya.pashapour@gmail.com>
2022-01-09 21:09:07 +0330

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
-var playerX = 185;
-var playerY = 185;
-var playerSpeed = 0.3;
+var playerX = 185; // The position of the player on the X axis
+var playerY = 185; // The position of the player on the Y axis
+var playerSpeed = 0.25; // The movement speed of the player
 
 var paused = false;
 
@@ -19,8 +19,9 @@ function setup() {
   createCanvas(400, 400);
   frameRate(60)
 
-  coinX = random(0, 370);
-  coinY = random(0, 370);
+  // Sets the coin's position to random coordinates
+  coinX = random(15, 385);
+  coinY = random(15, 385);
 }
 
 function draw() {
@@ -28,21 +29,25 @@ function draw() {
 
   fill(255, 255, 255)
 
+  // Updates the game if it is not paused
   if (!paused) {update();}
   else {text("Paused", 350, 10);}
 
+  // Clamps the player's position
+  // so it doesn't go off screen
   playerX = clamp(playerX, 15, 385);
   playerY = clamp(playerY, 15, 385);
 
-  //rect(playerX, playerY, 30, 30);
+  // Drawing the player
   ellipse(playerX, playerY, 30);
 
+  // Drawing the coin
   fill(252, 227, 3);
-  //rect(coinX, coinY, 20, 20);
   ellipse(coinX, coinY, 20);
 
+  // Drawing the score counter
   fill(255, 255, 255);
-  text("Coins: " + coins, 5, 10);
+  text("score: " + coins, 5, 10);
 }
 
 
@@ -51,6 +56,7 @@ function keyPressed() {
 }
 
 
+// Function that clamps a value
 function clamp(value, min, max) {
   var clamped
   if (value < min) {
@@ -66,6 +72,7 @@ function clamp(value, min, max) {
 }
 
 
+// Function that updates the game
 function update() {
   if (keyIsDown(KEY_W) || keyIsDown(UP_ARROW)) {
     playerY -= playerSpeed * deltaTime;
@@ -82,17 +89,17 @@ function update() {
 
   if (isColliding(playerX, playerY, 15, coinX, coinY, 10)) {
     coins += 1;
-    coinX = random(0, 370);
-    coinY = random(0, 370);
+    coinX = random(15, 385);
+    coinY = random(15, 385);
   }
 }
 
 
+// Function for checking collision between two circles
 function isColliding(x1, y1, r1, x2, y2, r2) {
   d = dist(x1, y1, x2, y2);
-  alert("E");
 
-  if (dist < r1 + r2) {
+  if (d < r1 + r2) {
     return true;
   }
   else {

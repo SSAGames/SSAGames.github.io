65906a562a0b985e32dad1cf606bbf70517e73d5
omthegg <iliya.pashapour@gmail.com>
2022-01-09 16:11:24 +0330

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 var playerX = 185
 var playerY = 185
-var playerSpeed = 1
+var playerSpeed = 0.5
 
 const KEY_W = 87;
 const KEY_S = 83;
@@ -16,20 +16,22 @@ function draw() {
   background(0, 0, 0)
 
   fill(255, 255, 255)
+
+  if (keyIsDown(KEY_W)) {
+    playerY -= playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_S)) {
+    playerY += playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_A)) {
+    playerX -= playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_D)) {
+    playerX += playerSpeed * deltaTime;
+  }
+
   rect(playerX, playerY, 30, 30)
 }
 
-function keyPressed() {
-  if (keyCode == KEY_W) {
-    playerY -= playerSpeed * deltaTime;
-  }
-  if (keyCode == KEY_S) {
-    playerY += playerSpeed * deltaTime;
-  }
-  if (keyCode == KEY_A) {
-    playerX -= playerSpeed * deltaTime;
-  }
-  if (keyCode == KEY_D) {
-    playerX += playerSpeed * deltaTime;
-  }
+function keyIsPressed() {
 }

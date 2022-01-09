09c9388ece3eb4befbaabb444f64fd7c21eb670d
omthegg <iliya.pashapour@gmail.com>
2022-01-09 19:10:02 +0330

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,11 +1,14 @@
-var playerX = 185
-var playerY = 185
-var playerSpeed = 0.5
+var playerX = 185;
+var playerY = 185;
+var playerSpeed = 0.5;
+
+var paused = false;
 
 const KEY_W = 87;
 const KEY_S = 83;
 const KEY_A = 65;
 const KEY_D = 68;
+const KEY_ESCAPE = 27;
 
 function setup() {
   createCanvas(400, 400);
@@ -17,24 +20,20 @@ function draw() {
 
   fill(255, 255, 255)
 
-  if (keyIsDown(KEY_W) || keyIsDown(UP_ARROW)) {
-    playerY -= playerSpeed * deltaTime;
-  }
-  if (keyIsDown(KEY_S) || keyIsDown(DOWN_ARROW)) {
-    playerY += playerSpeed * deltaTime;
-  }
-  if (keyIsDown(KEY_A) || keyIsDown(LEFT_ARROW)) {
-    playerX -= playerSpeed * deltaTime;
-  }
-  if (keyIsDown(KEY_D) || keyIsDown(RIGHT_ARROW)) {
-    playerX += playerSpeed * deltaTime;
-  }
+  if (!paused) {update();}
+  else {text("Paused", 10, 10);}
 
   playerX = clamp(playerX, 0, 370);
   playerY = clamp(playerY, 0, 370);
 
   rect(playerX, playerY, 30, 30)
 }
+
+
+function keyPressed() {
+  if (keyCode === KEY_ESCAPE) {paused = !paused;}
+}
+
 
 function clamp(value, min, max) {
   var clamped
@@ -48,4 +47,20 @@ function clamp(value, min, max) {
     clamped = value;
   }
   return clamped;
+}
+
+
+function update() {
+  if (keyIsDown(KEY_W) || keyIsDown(UP_ARROW)) {
+    playerY -= playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_S) || keyIsDown(DOWN_ARROW)) {
+    playerY += playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_A) || keyIsDown(LEFT_ARROW)) {
+    playerX -= playerSpeed * deltaTime;
+  }
+  if (keyIsDown(KEY_D) || keyIsDown(RIGHT_ARROW)) {
+    playerX += playerSpeed * deltaTime;
+  }
 }
